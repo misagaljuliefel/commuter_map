3f6eacd30dda7b45c60b77baea20745b72747366
juli.msgl <155543272+misagaljuliefel@users.noreply.github.com>
2026-08-17 02:32:35 +0000

    feat: optimize file streaming and format itinerary

--- route_finder.py
+++ route_finder.py
@@ -1,13 +1,23 @@
 import csv
 
+# --- SEARCHING & CHECKING (if existing) ROUTE
+
+def search_route(origin_input, dest_input):
+    try:
+        with open('community_routes.csv', mode='r') as input_file:
+            with open('my_itinerary.txt', 'w') as output_file:
+                reader = csv.DictReader(input_file)
+
+                for route_details in reader:
+                    if origin_input == route_details['Origin'].lower() and dest_input == route_details['Destination'].lower():
+                        output_file.write(f"Take a {route_details['Transport_Type']} to {route_details['Destination']}\nLoading Zone: {route_details['Loading_Zone']}\nPossible Fare: ₱{route_details['Fare']}\n\nTIPS!\n{route_details['Community_Tip']}")
+                    else:
+                        continue
+    except FileNotFoundError:
+        print("Database Missing!")
+
+
 current_location = input("Where you at?").lower()
 destination = input("Where to go?").lower()
 
-with open('community_routes.csv', mode='r') as file:
-    reader = csv.DictReader(file)
-
-    for route_details in reader:
-        if current_location == route_details['Origin'].lower() and destination == route_details['Destination'].lower():
-            print(route_details)
-        else:
-            continue
+search_route(current_location, destination)
