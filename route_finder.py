94d34e181273a61be3df90e2ec6de7787cf23158
juli.msgl <155543272+misagaljuliefel@users.noreply.github.com>
2026-08-17 01:17:58 +0000

    wrapped searching of routes in a reusable function

--- route_finder.py
+++ route_finder.py
@@ -1,13 +1,17 @@
 import csv
 
-current_location = input("Where you at?").lower()
-destination = input("Where to go?").lower()
+current_location = input("Where you at? ").lower()
+destination = input("Where to go? ").lower()
 
-with open('community_routes.csv', mode='r') as file:
-    reader = csv.DictReader(file)
+def search_route(origin_input, dest_input):
 
-    for route_details in reader:
-        if current_location == route_details['Origin'].lower() and destination == route_details['Destination'].lower():
-            print(route_details)
-        else:
-            continue
+    with open('community_routes.csv', mode='r') as file:
+        reader = csv.DictReader(file)
+
+        for route_details in reader:
+            if origin_input == route_details['Origin'].lower() and dest_input == route_details['Destination'].lower():
+                print(route_details)
+            else:
+                continue
+
+search_route(current_location, destination)
